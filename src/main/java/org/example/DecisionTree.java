91a0c6a48d462cebbbba31b9b7f5321a09bbaab5
MaleficKirby <ogupta4242@gmail.com>
2024-01-04 14:55:57 -0800

    init

---
+++ src/main/java/org/example/DecisionTree.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToNominal;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class DecisionTree {
+    public static void main(String[] args) throws Exception {
+        System.setErr(new PrintStream(new OutputStream() {public void write(int b) { /* Get rid of unused warning */ }}));
+
+        // Load data from CSV file
+        DataSource source = new DataSource("C:\\Users\\ruchi\\IdeaProjects\\MachineLearningTests\\src\\main\\java\\org\\example\\data.arff");
+        Instances data = source.getDataSet();
+
+
+        // Convert string attributes to nominal
+        StringToNominal stn = new StringToNominal();
+        stn.setInputFormat(data);
+        Instances newData = Filter.useFilter(data, stn);
+
+        // Set the class index
+        newData.setClassIndex(newData.numAttributes() - 1);
+
+        // Create J48 tree classifier
+        J48 tree = new J48();
+
+        // Train the classifier with the data
+        tree.buildClassifier(newData);
+
+        // Print out the built model
+        System.out.println(tree);
+    }
+}
---
+++ src/main/java/org/example/LoanClassifier.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import weka.classifiers.trees.RandomForest;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class LoanClassifier {
+    public static void main(String[] args) throws Exception {
+        System.setErr(new PrintStream(new OutputStream() {public void write(int b) { /* Get rid of unused warning */ }}));
+
+        // Load ARFF file
+        DataSource source = new DataSource("C:\\Users\\ruchi\\IdeaProjects\\MachineLearningTests\\src\\main\\java\\org\\example\\creditRisk.arff");
+        Instances data = source.getDataSet();
+
+        // Set class index (loan_grade)
+        data.setClassIndex(data.numAttributes() - 1);
+
+        // Create RandomForest classifier
+        RandomForest classifier = new RandomForest();
+        classifier.buildClassifier(data);
+
+        // Make predictions (replace with your own data)
+        double[] values = {22,59000,0,3};
+
+        // Create a new instance for prediction
+        Instance newInst = new DenseInstance(1.0, values);
+        newInst.setDataset(data);
+
+        // Get predicted class label
+        int predictedClass = (int) classifier.classifyInstance(newInst);
+        String predictedClassLabel = data.classAttribute().value(predictedClass);
+
+        // Print the predicted loan grade
+        System.out.println("Predicted Loan Grade: " + predictedClassLabel);
+        System.out.println(classifier);
+    }
+}
